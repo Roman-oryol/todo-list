61178c0e6c4cdc2ff84383da6e66ed69d096361a
Roman Oryol <roman.oryol83@gmail.com>
2025-04-07 09:47:10 +0300

    Добавить функцию для генерации разметки деталей задачи

---
+++ src/js/components/task-details.js
@@ -0,0 +1,29 @@
+/**
+ * Generates the markup for displaying the details of a task.
+ *
+ * @param {Object} task - The task object containing details to be displayed.
+ * @returns {string} The HTML markup string for the task details.
+ */
+
+export function createTaskDetailsMarkup(task) {
+  const { title, description, dueDate, priority, status } = task;
+  return `
+    <header class="task-header">
+            <h2 class="task-title">${title}</h2>
+            <p class="task-priority">
+              Приоритет: <span class="${priority}">${priority}</span>
+            </p>
+            <p class="task-status">
+              Статус: <span class="${status}">${status
+    .split('-')
+    .join(' ')}</span>
+            </p>
+            <p class="task-due-date">
+              Срок: <span class="due-date">${dueDate}</span>
+            </p>
+          </header>
+          <div class="task-info">
+            <p class="task-text">${description}</p>
+          </div>
+  `;
+}
