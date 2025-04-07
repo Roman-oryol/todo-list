6bf92f32a615c10a938d244c3bfae3db9e33fee8
Roman Oryol <roman.oryol83@gmail.com>
2025-04-07 09:48:19 +0300

    Добавить класс List для управления списком задач

---
+++ src/js/modules/list.js
@@ -0,0 +1,52 @@
+import { v4 as uuidv4 } from 'uuid';
+import TaskManager from './task-manager';
+
+export default class List {
+  isActive = true;
+  isEmpty = true;
+  #taskManager = new TaskManager();
+
+  constructor(name) {
+    this.id = uuidv4();
+    this.name = name;
+  }
+
+  toggleActive(isActive) {
+    this.isActive = isActive;
+  }
+
+  addTask(task) {
+    this.#taskManager.addTask(task);
+    this.isEmpty = false;
+  }
+
+  get taskList() {
+    return this.#taskManager.getTasks();
+  }
+
+  setTaskSelected(task) {
+    this.#taskManager.setTaskSelected(task);
+  }
+
+  deleteTask(task) {
+    if (this.isEmpty) {
+      return;
+    }
+
+    this.#taskManager.deleteTask(task);
+
+    if (this.isTaskListEmpty()) {
+      this.isEmpty = true;
+    } else {
+      this.setTaskSelected(this.taskList[0]);
+    }
+  }
+
+  isTaskListEmpty() {
+    return this.taskList.length === 0;
+  }
+
+  getActiveTask() {
+    return this.taskList.find((task) => task.isSelected);
+  }
+}
