ad7195926167dccdc21083719d79d70126bb6461
Roman Oryol <roman.oryol83@gmail.com>
2025-04-07 09:49:03 +0300

    Добавить класс Task для представления задач

---
+++ src/js/modules/task.js
@@ -0,0 +1,20 @@
+import { v4 as uuidv4 } from 'uuid';
+
+class Task {
+  isSelected = false;
+
+  constructor({ title, description, date, priority, status = 'not-started' }) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = date;
+    this.priority = priority;
+    this.status = status;
+    this.id = uuidv4();
+  }
+
+  toggleSelection(isSelected) {
+    this.isSelected = isSelected;
+  }
+}
+
+export default Task;
