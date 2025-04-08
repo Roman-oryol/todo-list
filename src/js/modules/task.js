7302f89c8b61c9b2e762936ff4566b989dacac49
Roman Oryol <roman.oryol83@gmail.com>
2025-04-08 10:38:23 +0300

    Добавить метод редактирования задачи в класс Task

--- src/js/modules/task.js
+++ src/js/modules/task.js
@@ -15,6 +15,13 @@ class Task {
   toggleSelection(isSelected) {
     this.isSelected = isSelected;
   }
+
+  editTask({ title, description, date, priority }) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = date;
+    this.priority = priority;
+  }
 }
 
 export default Task;
