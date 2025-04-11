4e12f6721e29a3ce73fa1c255c259982db83966c
Roman Oryol <roman.oryol83@gmail.com>
2025-04-11 08:55:33 +0300

    Добавить контроллер изменения статуса задач

---
+++ src/js/controllers/change-status-controller.js
@@ -0,0 +1,77 @@
+import { getActiveList } from '../modules/task-list-manager';
+import { renderTaskDetails, renderTasks } from './ui-controller';
+
+const changeStatusModal = document.getElementById('change-status-dialog');
+const modalContent = changeStatusModal.querySelector('.modal-content');
+const changeStatusBtn =
+  changeStatusModal.querySelectorAll('.change-status-btn');
+
+function highlightCurrentStatus(task) {
+  const currentStatusElement = changeStatusModal.querySelector(
+    `[data-status="${task.status}"]`
+  );
+  if (currentStatusElement) {
+    changeStatusBtn.forEach((btn) => {
+      btn.disabled = false;
+      btn.classList.remove('highlight');
+    });
+    currentStatusElement.disabled = true;
+    currentStatusElement.classList.add('highlight');
+  }
+}
+
+function handleChangeStatus(task, newStatus) {
+  const activeList = getActiveList();
+  const taskForChange = activeList.taskList.find((t) => t.id === task.id);
+
+  console.log(taskForChange);
+  taskForChange.changeStatus(newStatus);
+  renderTasks(getActiveList().taskList);
+
+  if (taskForChange.isSelected) {
+    renderTaskDetails(task);
+  }
+
+  changeStatusModal.close();
+  console.log(getActiveList().taskList);
+  console.log('newStatus: ', newStatus);
+}
+
+function openChangeStatusModal(task) {
+  highlightCurrentStatus(task);
+
+  const handleButtonClick = (event) =>
+    handleChangeStatus(task, event.target.dataset.status);
+
+  const handleOutsideClick = (event) => {
+    if (!modalContent.contains(event.target)) {
+      closeChangeStatusModal();
+      document.removeEventListener('click', handleOutsideClick);
+    }
+  };
+
+  changeStatusBtn.forEach((btn) => {
+    btn.addEventListener('click', handleButtonClick);
+  });
+
+  changeStatusModal.addEventListener('close', function cleanup() {
+    changeStatusBtn.forEach((btn) => {
+      btn.removeEventListener('click', handleButtonClick);
+    });
+    changeStatusModal.removeEventListener('close', cleanup);
+    document.removeEventListener('click', handleOutsideClick);
+  });
+
+  document.addEventListener('click', handleOutsideClick);
+  changeStatusModal.showModal();
+}
+
+function closeChangeStatusModal() {
+  changeStatusModal.close();
+}
+
+export {
+  openChangeStatusModal,
+  closeChangeStatusModal,
+  highlightCurrentStatus,
+};
